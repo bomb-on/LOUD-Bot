a590a6ac94c074d845fe6fc5591abe84e216b055
LOUD Mining <39448763+LOUD-Mining@users.noreply.github.com>
2019-01-28 21:02:39 -0600

    Update beer.js

--- commands/beer.js
+++ commands/beer.js
@@ -1,8 +1,9 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let check = message.channel.id;
-    let correct = "537913904005775361"
-    if(check == correct || message.channel.type == "dm") {
+    let correct = "537913904005775361";
+    if(message.channel.type == "dm") return message.channel.send("Sorry can't do that here!");
+    if(check == correct) {
     return message.channel.send(`Have a cold one ${message.author.username}`, {file: "https://webcn.loudmining.com/loudbot/img/beer.png"});
     }
     message.channel.bulkDelete(1);
