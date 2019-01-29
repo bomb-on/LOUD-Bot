e284e1fea8f73fa6c6dd9493520463baf6805c26
LOUD Mining <39448763+LOUD-Mining@users.noreply.github.com>
2019-01-28 21:03:45 -0600

    Update coffee.js

--- commands/coffee.js
+++ commands/coffee.js
@@ -1,8 +1,9 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let check = message.channel.id;
-    let correct = "537913904005775361"
-    if(check == correct || message.channel.type == "dm") {
+    let correct = "537913904005775361";
+    if(message.channel.type == "dm") return message.channel.send("Sorry can't do that here!");
+    if(check == correct) {
     message.channel.send(`Here's your coffee ${message.author.username}`, {file: "https://webcn.loudmining.com/loudbot/img/coffeespill.jpg"});
     setTimeout(function() {
         message.channel.send("***Awww man!! Now look what you made me do!!***");
