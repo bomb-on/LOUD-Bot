2ec7681be60ac4ecab32ada98c5f0e32f1de5905
LOUD Mining <39448763+LOUD-Mining@users.noreply.github.com>
2019-02-06 00:03:20 -0600

    Update software.js

--- commands/software.js
+++ commands/software.js
@@ -1,36 +1,51 @@
 const loudFoot = require("./configs/footer.json");
 const Discord = require("discord.js");
-const vrsc = "vrsc";
-const web = "web";
 module.exports.run = async (bot, message, args) => {
     let check = message.channel.id;
     let correct = "537913904005775361";
-    if(message.channel.type == "dm") return message.channel.send("Sorry can't do that here!");
+    if (message.channel.type == "dm") {
+        return message.channel.send("Sorry can't do that here!");
+    }
     if(check == correct) {
+        if (!args[0]) {
+            return message.reply("Aww man...something went wrong...\n\nTry **vrsc** or **web** after your command.");
+        }
         var title;
         var cpuMiners;
-        var gpuMiners;
-        var wallet;
-        if(!args[0]) return message.reply("Aww man...something went wrong...\n\nTry **vrsc** or **web** after your command.");
-        if(args[0] != vrsc || args[0] != web) return message.reply("Ok quit messing around! :joy:");
-        if(args[0] = vrsc) {
+        var nvidiaGpuMiners;
+        var amdGpuMiners;
+        var guiWallet;
+        if (args[0] == "vrsc") {
             title = "VerusCoin";
-            cpuMiners = `**CPU ONLY**\n\n**CCminer**\nhttps://github.com/monkins1010/ccminer/releases/tag/v3.3CPU \n\n**nheqminer**\nhttps://github.com/VerusCoin/nheqminer/releases`;
-            gpuMiners = `**NVIDIA**\n\n**CCminer**\nhttps://github.com/monkins1010/ccminer/releases \n\n**AMD**\n\n***Coming Soon! Please stay tuned!***`;
-            wallet = `**GUI-Wallet**\n\n**Agama**\nhttps://github.com/VerusCoin/Agama/releases \n\n**CLI-Wallet**\n\n**VerusCoin**\nhttps://github.com/VerusCoin/VerusCoin/releases`;
-        } else if (args[0] = web) {
+            cpuMiners = `CCminer\nhttps://github.com/monkins1010/ccminer/releases/tag/v3.3CPU \nnheqminer\nhttps://github.com/VerusCoin/nheqminer/releases`;
+            nvidiaGpuMiners = `CCminer\nhttps://github.com/monkins1010/ccminer/releases`;
+            amdGpuMiners = `Coming Soon! Please stay tuned!`;
+            guiWallet = `Agama\nhttps://github.com/VerusCoin/Agama/releases`;
+            cliWallet = `VerusCoin\nhttps://github.com/VerusCoin/VerusCoin/releases`;
+        } else if (args[0] == "web") {
             title = "Webchain";
-            cpuMiners = `**CPU ONLY**\n\n**0% Dev Fee Courtesty of LOUD Mining**\n***Pre-Compiled*** - https://webcn.loudmining.com/#/downloads \n\n***Source Code*** - https://github.com/LOUD-Mining/webchain-miner \n\n**Webchain-miner**\n**Developers Github** - https://github.com/webchain-network/webchain-miner`;
-            gpuMiners = `**Nvidia**\n\n**xm-rig**\n***Pre-Compiled***\nhttps://webcn.loudmining.com/#/downloads \n**Source Code**\nhttps://github.com/monkins1010/Webchain-nvidia-opensource \n\n**AMD**\n\n**xm-rig**\nhttps://github.com/monkins1010/AMDWEBCHAIN-AMD/releases \n**xmr-stak**\nhttps://github.com/monkins1010/LOUD-Webchain-xmr-stak-AMD/releases`;
-            wallet = `**GUI-Wallet**\n\n**webchain-wallet**\nhttps://github.com/webchain-network/webchain-wallet/releases \n\n**CLI-Wallet**\n\n**webchaind**\nhttps://github.com/webchain-network/webchaind/releases`;
+            cpuMiners = `*0% Dev Fee* Courtesty of LOUD Mining\nPre-Compiled - https://webcn.loudmining.com/#/downloads \nSource Code - https://github.com/LOUD-Mining/webchain-miner \nWebchain-miner\nDevelopers Github - https://github.com/webchain-network/webchain-miner`;
+            nvidiaGpuMiners = `xm-rig\nPre-Compiled\nhttps://webcn.loudmining.com/#/downloads \nSource Code\nhttps://github.com/monkins1010/Webchain-nvidia-opensource`;
+            amdGpuMiners = `xm-rig\nhttps://github.com/monkins1010/AMDWEBCHAIN-AMD/releases \nxmr-stak\nhttps://github.com/monkins1010/LOUD-Webchain-xmr-stak-AMD/releases`;
+            guiWallet = `webchain-wallet\nhttps://github.com/webchain-network/webchain-wallet/releases`;
+            cliWallet = `webchaind\nhttps://github.com/webchain-network/webchaind/releases`;
+        } else if (args[0] !== "web") { 
+            if (args[0] !== "vrsc") {
+                return message.reply("Quit messin around :rolling_eyes:")
+            }
+            return;
         }
         let softwareEmbed = new Discord.RichEmbed()
-        .setTitle(`${title}`)
+        .setTitle(title)
         .setColor("#00720D")
         .setThumbnail(URL = "https://loudmining.com/images/loudog.png")
-        .addField("Mining Software", (cpuMiners, gpuMiners))
-        .addField("Wallet Software", (wallet))
-        .addField("Powered by © LOUD Mining 2018", (loudFoot.footer));
+        .addField("GUI-Wallet", guiWallet)
+        .addField("CLI-Wallet", cliWallet)
+        .addField("CPU Miners", cpuMiners)
+        .addField("GPU Miners", "------------")
+        .addField("AMD", amdGpuMiners)
+        .addField("NVidia",nvidiaGpuMiners)
+        .addField("Powered by © LOUD Mining 2018", loudFoot.footer);
         return message.channel.send(softwareEmbed);
     }
     message.channel.bulkDelete(1);
